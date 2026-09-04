58a3cdec42d8abdd019f0ab48957095d40e602dd
Anwesha Mohapatra <anweshamohapatra2005@gmail.com>
2026-09-04 22:27:27 +0530

    Time: 267 ms (70.44%), Space: 280.9 MB (86.66%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -3,25 +3,27 @@ class Solution {
  
 public:
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        vector<vector<int>>graph(n);
-        for(auto &e:edges){
-            graph[e[0]].push_back(e[1]);
-            graph[e[1]].push_back(e[0]);
+       vector<vector<int>>graph(n);
+       for(auto & edge:edges){
+        graph[edge[0]].push_back(edge[1]);
+        graph[edge[1]].push_back(edge[0]);
+       }
+       vector<bool> visited(n,false);
+       stack<int> st;
+       st.push(source);
+       while(!st.empty()){
+      int node=st.top();
+      if(node==destination)
+      return true;
+      st.pop();
+        if(visited[node])
+        continue;
+        visited[node]=true;
+        for(auto &n:graph[node]){
+            if(!visited[n])
+            st.push(n);
         }
-        vector<bool> visited(n,false);
-        stack<int>st;
-        st.push(source);
-        while(!st.empty()){
-int node=st.top();st.pop();
-if(node==destination) 
-return true;
-if(visited[node]){
-continue;}
-visited[node]=true;
-for(int n:graph[node]){
-    if(!visited[n])st.push(n);
-}
-        }
-        return false;
+       }
+       return false;
     }
 };
